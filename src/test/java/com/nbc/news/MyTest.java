6c6a311a76879f9952d6806003a8330c08231fbf
Thai <thai.dangvu@nbcuni.com>
2023-05-04 10:56:48 -0400

    test GitHub Actions

--- src/test/java/com/nbc/news/MyTest.java
+++ src/test/java/com/nbc/news/MyTest.java
@@ -1,10 +1,15 @@
 package com.nbc.news;
 
-import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MyTest {
   @Test
   void test() {
+    int[] inputs = { 1, 9, 8, 2, 4, 1, 9 };
+    int[] outputs = IntStream.of(inputs).map(i -> i + i).map(i -> i / 2).toArray();
+    Assertions.assertArrayEquals(inputs, outputs, "Hi hi");
   }
 }
